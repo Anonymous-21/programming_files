1d6cd073227407d33dbc8b9329e5522b228c2c75
Anshul Joshi <anshuljoshi21@gmail.com>
2024-10-02 14:22:32 +0530

    changes in pong game - game.py, paddle.py

--- python_files/raylib/pong/game.py
+++ python_files/raylib/pong/game.py
@@ -1,7 +1,7 @@
 import raylib as r
 
 from encode import encode
-from paddle import paddle_left, paddle_right
+from paddle import Paddle
 from ball import Ball
 
 SCREEN_WIDTH = 800
@@ -13,19 +13,27 @@ GAME_FPS = 60
 
 class Game:
     def __init__(self) -> None:
-        self.paddle_left = paddle_left
-        self.paddle_right = paddle_right
+        self.game_over = False
+        
+        self.paddle = Paddle()
         self.ball = Ball()
         
     def draw(self):
-        self.paddle_left.draw()
-        self.paddle_right.draw()
+        # screen divider
+        r.DrawLineEx((r.GetScreenWidth()//2,
+                   0),
+                   (r.GetScreenWidth()//2,
+                   r.GetScreenHeight()),
+                   2,
+                   r.GRAY)
+        
+        self.paddle.draw()
         self.ball.draw()
         
     def update(self):
-        self.paddle_left.move(r.KEY_W, r.KEY_S)
-        self.paddle_right.move(r.KEY_UP, r.KEY_DOWN)
-        self.ball.move()
+        if not self.game_over:
+            self.paddle.move()
+            self.ball.move()
         
 
 def main():
--- python_files/raylib/pong/paddle.py
+++ python_files/raylib/pong/paddle.py
@@ -2,47 +2,41 @@ import raylib as r
 
 
 class Paddle:
-    def __init__(self, x, y, width, height, color, speed) -> None:
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.speed = speed
+    def __init__(self) -> None:
+        self.width = 10
+        self.height = 100
+        self.left_x = 10
+        self.right_x = r.GetScreenWidth() - self.width - 10
+        self.initial_y = r.GetScreenHeight()//2 - self.height//2
+        self.left_y = self.initial_y
+        self.right_y = self.initial_y
+        self.color = r.BLACK
+        self.speed = 5
 
     def draw(self):
-        r.DrawRectangle(self.x,
-                        self.y,
+        # draw left paddle
+        r.DrawRectangle(self.left_x,
+                        self.left_y,
+                        self.width,
+                        self.height,
+                        self.color)
+        # draw right paddle
+        r.DrawRectangle(self.right_x,
+                        self.right_y,
                         self.width,
                         self.height,
                         self.color)
     
-    def move(self, key_up, key_down):
-        if r.IsKeyDown(key_up) and self.y >= 0:
-            self.y -= self.speed
-        elif r.IsKeyDown(key_down) and self.y <= r.GetScreenHeight() - self.height:
-            self.y += self.speed
+    def move(self):
+        # left paddle
+        if r.IsKeyDown(r.KEY_W) and self.left_y >= 0:
+            self.left_y -= self.speed
+        elif r.IsKeyDown(r.KEY_S) and self.left_y <= r.GetScreenHeight() - self.height:
+            self.left_y += self.speed
+        
+        # right paddle
+        if r.IsKeyDown(r.KEY_UP) and self.right_y >= 0:
+            self.right_y -= self.speed
+        elif r.IsKeyDown(r.KEY_DOWN) and self.right_y <= r.GetScreenHeight() - self.height:
+            self.right_y += self.speed
             
-
-WIDTH = 10
-HEIGHT = 100
-COLOR = r.BLACK
-SPEED = 4
-
-LEFT_X = 10
-RIGHT_X = r.GetScreenWidth() - WIDTH - 10
-Y = r.GetScreenHeight()//2 
-
-paddle_left = Paddle(x=LEFT_X,
-                     y=Y,
-                     width=WIDTH,
-                     height=HEIGHT,
-                     color=COLOR,
-                     speed=SPEED)
-
-paddle_right = Paddle(x=RIGHT_X,
-                     y=Y,
-                     width=WIDTH,
-                     height=HEIGHT,
-                     color=COLOR,
-                     speed=SPEED)
