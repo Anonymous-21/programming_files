eda6f9ab40088fe62d9fafb4c15445832efccc0e
Anshul Joshi <anshuljoshi21@gmail.com>
2024-10-03 00:33:27 +0530

    platformer_update

--- python_files/raylib/platformer/player.py
+++ python_files/raylib/platformer/player.py
@@ -2,73 +2,90 @@ import raylib as r
 
 
 class Player:
-    def __init__(self, sprite_dict) -> None:
+    def __init__(self, sprite_dict, spritesheet) -> None:
+        self.spritesheet = spritesheet
         self.sprite_dict = sprite_dict
+        self.frames_counter = 0
+        self.frames_speed = 8
+        self.frame_num = 1
+        self.current_frame = sprite_dict[self.frame_num]
+        
         self.alien_biege = {
-            "climb1": "alienBeige_climb1.png",
-            "climb2": "alienBeige_climb2.png",
-            "duck": "alienBeige_duck.png",
-            "front": "alienBeige_front.png",
-            "hit": "alienBeige_hit.png",
-            "jump": "alienBeige_jump.png",
-            "stand": "alienBeige_stand.png",
-            "swim1": "alienBeige_swim1.png",
-            "swim2": "alienBeige_swim2.png",
-            "walk1": "alienBeige_walk1.png",
-            "walk2": "alienBeige_walk2.png",
+            1: "alienBeige_front.png",
+            2: "alienBeige_stand.png",
+            3: "alienBeige_walk1.png",
+            4: "alienBeige_walk2.png",
+            5: "alienBeige_jump.png",
+            6: "alienBeige_duck.png",
+            7: "alienBeige_hit.png",
+            8: "alienBeige_climb1.png",
+            9: "alienBeige_climb2.png",
+            10: "alienBeige_swim1.png",
+            11: "alienBeige_swim2.png",
         }
         self.alien_blue = {
-            "climb1": "alienBlue_climb1.png",
-            "climb2": "alienBlue_climb2.png",
-            "duck": "alienBlue_duck.png",
-            "front": "alienBlue_front.png",
-            "hit": "alienBlue_hit.png",
-            "jump": "alienBlue_jump.png",
-            "stand": "alienBlue_stand.png",
-            "swim1": "alienBlue_swim1.png",
-            "swim2": "alienBlue_swim2.png",
-            "walk1": "alienBlue_walk1.png",
-            "walk2": "alienBlue_walk2.png",
+            1: "alienBlue_front.png",
+            2: "alienBlue_stand.png",
+            3: "alienBlue_walk1.png",
+            4: "alienBlue_walk2.png",
+            5: "alienBlue_jump.png",
+            6: "alienBlue_duck.png",
+            7: "alienBlue_hit.png",
+            8: "alienBlue_climb1.png",
+            9: "alienBlue_climb2.png",
+            10: "alienBlue_swim1.png",
+            11: "alienBlue_swim2.png",
         }
         self.alien_green = {
-            "climb1": "alienGreen_climb1.png",
-            "climb2": "alienGreen_climb2.png",
-            "duck": "alienGreen_duck.png",
-            "front": "alienGreen_front.png",
-            "hit": "alienGreen_hit.png",
-            "jump": "alienGreen_jump.png",
-            "stand": "alienGreen_stand.png",
-            "swim1": "alienGreen_swim1.png",
-            "swim2": "alienGreen_swim2.png",
-            "walk1": "alienGreen_walk1.png",
-            "walk2": "alienGreen_walk2.png",
+            1: "alienGreen_front.png",
+            2: "alienGreen_stand.png",
+            3: "alienGreen_walk1.png",
+            4: "alienGreen_walk2.png",
+            5: "alienGreen_jump.png",
+            6: "alienGreen_duck.png",
+            7: "alienGreen_hit.png",
+            8: "alienGreen_climb1.png",
+            9: "alienGreen_climb2.png",
+            10: "alienGreen_swim1.png",
+            11: "alienGreen_swim2.png",
         }
         self.alien_pink = {
-            "climb1": "alienPink_climb1.png",
-            "climb2": "alienPink_climb2.png",
-            "duck": "alienPink_duck.png",
-            "front": "alienPink_front.png",
-            "hit": "alienPink_hit.png",
-            "jump": "alienPink_jump.png",
-            "stand": "alienPink_stand.png",
-            "swim1": "alienPink_swim1.png",
-            "swim2": "alienPink_swim2.png",
-            "walk1": "alienPink_walk1.png",
-            "walk2": "alienPink_walk2.png",
+            1: "alienPink_front.png",
+            2: "alienPink_stand.png",
+            3: "alienPink_walk1.png",
+            4: "alienPink_walk2.png",
+            5: "alienPink_jump.png",
+            6: "alienPink_duck.png",
+            7: "alienPink_hit.png",
+            8: "alienPink_climb1.png",
+            9: "alienPink_climb2.png",
+            10: "alienPink_swim1.png",
+            11: "alienPink_swim2.png",
         }
         self.alien_yellow = {
-            "climb1": "alienYellow_climb1.png",
-            "climb2": "alienYellow_climb2.png",
-            "duck": "alienYellow_duck.png",
-            "front": "alienYellow_front.png",
-            "hit": "alienYellow_hit.png",
-            "jump": "alienYellow_jump.png",
-            "stand": "alienYellow_stand.png",
-            "swim1": "alienYellow_swim1.png",
-            "swim2": "alienYellow_swim2.png",
-            "walk1": "alienYellow_walk1.png",
-            "walk2": "alienYellow_walk2.png",
+            1: "alienYellow_front.png",
+            2: "alienYellow_stand.png",
+            3: "alienYellow_walk1.png",
+            4: "alienYellow_walk2.png",
+            5: "alienYellow_jump.png",
+            6: "alienYellow_duck.png",
+            7: "alienYellow_hit.png",
+            8: "alienYellow_climb1.png",
+            9: "alienYellow_climb2.png",
+            10: "alienYellow_swim1.png",
+            11: "alienYellow_swim2.png",
         }
 
     def draw(self):
-        pass
+        r.DrawTextureRec(self.spritesheet,
+                         self.sprite_dict[self.current_frame],
+                         (r.GetScreenWidth()//2,
+                          r.GetScreenHeight()//2),
+                         r.WHITE)
+        
+    def update(self):
+        self.frames_counter += 1
+        if self.frames_counter % self.frames_speed:
+            self.frame_num += 1
+            if self.frame_num > 11:
+                self.frame_num = 1
