aa2b1a13884352b7b5921060f25ecc14d56a3da0
Anshul Joshi <anshuljoshi21@gmail.com>
2024-10-22 12:24:01 +0530

    simple_platformer updates

--- c_files/raylib/simple_platformer/levels.h
+++ c_files/raylib/simple_platformer/levels.h
@@ -6,16 +6,26 @@
 #define ROW 20
 #define COL 20
 
-typedef struct Levels{
+typedef struct Levels {
 
   int current_level;
   int block_size;
   int (*level)[COL];
-  int level1[ROW][COL];
-  int level2[ROW][COL];
-  int level3[ROW][COL];
-  int level4[ROW][COL];
-  int level5[ROW][COL];
+  int level1_locked[ROW][COL];
+  int level1_unlocked[ROW][COL];
+  bool level1_pass;
+  int level2_locked[ROW][COL];
+  int level2_unlocked[ROW][COL];
+  bool level2_pass;
+  int level3_locked[ROW][COL];
+  int level3_unlocked[ROW][COL];
+  bool level3_pass;
+  int level4_locked[ROW][COL];
+  int level4_unlocked[ROW][COL];
+  bool level4_pass;
+  int level5_locked[ROW][COL];
+  int level5_unlocked[ROW][COL];
+  bool level5_pass;
 
 } Levels;
 
