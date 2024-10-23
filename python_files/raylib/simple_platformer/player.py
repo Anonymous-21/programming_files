90edeaa668b6e2a85275660f4eec638eda9e9102
Anshul Joshi <anshuljoshi21@gmail.com>
2024-10-23 21:25:03 +0530

    updates

--- python_files/raylib/simple_platformer/player.py
+++ python_files/raylib/simple_platformer/player.py
@@ -6,11 +6,39 @@ class Player:
         self.sprite_dict = sprite_dict
 
         self.player_idle = self.sprite_dict["tile_0045.png"]
+        
+        self.x_window = 20
+        self.y_window = p.get_screen_height() - 95
+        self.change_x = 5
+        self.change_y = 0
+        self.gravity = 1
+        self.jump_force = -20
+        self.can_jump = True
+        
+        self.current_frame = self.player_idle
+        self.current_frame_width = self.current_frame[2]
+        self.current_frame_height = self.current_frame[3]
 
     def draw(self):
         p.draw_texture_pro(self.spritesheet,
-                           (self.player_idle[0], self.player_idle[1], self.player_idle[2], self.player_idle[3]),
-                           (20, p.get_screen_height() - 90, self.player_idle[2] * 3, self.player_idle[3] * 3),
-                           (self.player_idle[2]/2, self.player_idle[3]/2),
+                           (self.current_frame[0], self.current_frame[1], self.current_frame_width, self.current_frame_height),
+                           (self.x_window, self.y_window, self.current_frame_width * 3, self.current_frame_height * 3),
+                           (self.current_frame_width/2, self.current_frame_height/2),
                            0,
                            p.WHITE)
+        
+    def update(self):
+        # horizontal movement
+        if p.is_key_down(p.KeyboardKey.KEY_RIGHT):
+            self.x_window += self.change_x
+        elif p.is_key_down(p.KeyboardKey.KEY_LEFT):
+            self.x_window -= self.change_x
+            
+        # vertical movement
+        if p.is_key_pressed(p.KeyboardKey.KEY_SPACE) and self.can_jump:
+            self.change_y = self.jump_force
+            self.can_jump = False
+        
+        self.change_y += self.gravity
+        self.y_window += self.change_y
+        
